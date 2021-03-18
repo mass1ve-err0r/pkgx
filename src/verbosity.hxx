f0871a2c866db9af61e78ae544050b3fe84cb714
mass1ve-err0r <me@saadat.dev>
2021-03-18 16:56:27 +0100

    add globul header + this is just for the verbose ouputt

---
+++ src/verbosity.hxx
@@ -0,0 +1,14 @@
+/*
+ * Author : Mirza Saadat Baig, <me@saadat.dev>
+ * Date   : 18.03.21
+ * License: BSD-3-Clause-Attribution
+ */
+#ifndef PKGX_VERBOSITY_HXX
+#define PKGX_VERBOSITY_HXX
+
+// --------------------*  globals  *--------------------
+
+inline bool VERBOSE_OUTPUT_ENABLED = false;
+
+
+#endif //PKGX_VERBOSITY_HXX
