cabf934234c91e7127c0ed5b02afde548533512b
mass1ve-err0r <me@saadat.dev>
2021-03-18 16:44:41 +0100

    add compactarchive spec ==> This is in all essence my own idea of how the old `ar` archive should be. Please refer to this folder's README for full specification & details.
    ==> Some funcs are entirely unused because I haven't had the need for them at the moment, as in, a standalone compactarchive is currently not doable with these files (as in, this won't output on its own). Will fledge this out at some later stage into it's own thing perhaps
    
    (!) Logging is still messy, please bear with me lmao

---
+++ src/utils/compact_archive.cxx
@@ -0,0 +1,85 @@
+/*
+ * Author : Mirza Saadat Baig, <me@saadat.dev>
+ * Date   : 09.03.21
+ * License: BSD-3-Clause-Attribution
+ */
+#include "compact_archive.hxx"
+
+
+int pack(const std::vector<std::pair<std::string, uint32_t>>& files, const std::string& dest)
+{
+    /* not implemented yet (unused) */
+    return 0;
+}
+
+int unpack(char* compactarchive_buf, const std::string& dest)
+{
+    /* not implemented yet (unused) */
+    return 0;
+}
+
+char* pack_to_buffer(const std::vector<std::pair<std::string, uint32_t>>& files, size_t* buf_sz)
+{
+    uint32_t total_sz = 0;
+    uint32_t pos = 0;
+    for(auto& i: files) {
+        total_sz += 4;
+        total_sz += i.second;
+    }
+    uint32_t elements = files.size();
+    *buf_sz = total_sz + 4;
+    char* buffer = new(std::nothrow) char[total_sz+4];
+    if (!buffer) {
+        LOG(ERROR, "Failed to create buffer!");
+        return NULL;
+    }
+    memcpy(buffer+pos, &elements, sizeof(uint32_t));
+    pos += 4;
+    for(auto& entry: files) {
+        FILE* current_file = fopen(entry.first.c_str(), "rb");
+        if (!current_file) {
+            LOG(ERROR, "Could NOT open file to buffer!");
+            delete[] buffer;
+            return NULL;
+        }
+        memcpy(buffer+pos, &entry.second, sizeof(uint32_t));
+        pos += 4;
+        fread(buffer+pos, entry.second, 1, current_file);
+        pos += entry.second;
+        fclose(current_file);
+    }
+    return buffer;
+}
+
+char** unpack_to_buffer(char* compactarchive_buf, size_t* buf_sz, std::vector<size_t>& sizes)
+{
+    size_t idx = 0;
+    size_t bpos = 0;
+    uint32_t sz;
+    uint32_t elements;
+    memcpy(&elements, compactarchive_buf+bpos, sizeof(uint32_t));
+    if (VERBOSE_OUTPUT_ENABLED) { LOG(DEBUG, "Elements inside: ", elements); }
+    bpos += 4;
+    char** buf = new char*[elements];
+    while (true) {
+        if (bpos >= *buf_sz) {
+            break;
+        }
+        sz = 0;
+        memcpy(&sz, compactarchive_buf+bpos, sizeof(uint32_t));
+        if (VERBOSE_OUTPUT_ENABLED) { LOG(DEBUG, "Current element size: ", sz); }
+        bpos += 4;
+        buf[idx] = new char[sz];
+        if (!buf[idx]) {
+            LOG(ERROR, "Could not create buffer for current element!");
+            for(int i = 0; i < elements; i++) { delete[] buf[i]; }
+            delete[] buf;
+            return NULL;
+        }
+        memcpy(buf[idx], compactarchive_buf+bpos, sz);
+        bpos += sz;
+        idx++;
+        sizes.emplace_back(sz);
+    }
+    return buf;
+}
---
+++ src/utils/compact_archive.hxx
@@ -0,0 +1,24 @@
+/*
+ * Author : Mirza Saadat Baig, <me@saadat.dev>
+ * Date   : 09.03.21
+ * License: BSD-3-Clause-Attribution
+ */
+#ifndef PKGX_COMPACT_ARCHIVE_HXX
+#define PKGX_COMPACT_ARCHIVE_HXX
+
+// --------------------*  imports  *--------------------
+#include "../verbosity.hxx"
+#include "../helpers/terminal.hxx"
+#include <cstdint>
+#include <vector>
+#include <string>
+
+
+// --------------------*  funcs  *--------------------
+int pack(const std::vector<std::pair<std::string, uint32_t>>& files, const std::string& dest);
+int unpack(char* compactarchive_buf, const std::string& dest);
+char* pack_to_buffer(const std::vector<std::pair<std::string, uint32_t>>& files, size_t* buf_sz);
+char** unpack_to_buffer(char* compactarchive_buf, size_t* buf_sz, std::vector<size_t>& sizes);
+
+
+#endif //PKGX_COMPACT_ARCHIVE_HXX
